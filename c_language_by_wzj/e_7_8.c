2507bb1c7057af0089907ac886c70c5322701f46
freepose <freepose@126.com>
2025-04-25 08:37:48 +0800

    update

---
+++ c_language_by_wzj/e_7_8.c
@@ -0,0 +1,18 @@
+﻿
+// 字符数组
+
+#include <stdio.h>
+
+int main()
+{
+    // char buffer[20] = {'H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd', '\0'}; 
+    // for (int i = 0; i < 20; i++)
+    // {
+    //     printf("%c", buffer[i]);
+    // }
+
+    char *buffer2 = "Hello World"; // 字符串常量
+    printf("%s\n", buffer2);
+
+    return 0;
+}
