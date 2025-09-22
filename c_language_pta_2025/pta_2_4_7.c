9b49feba1ff8dc3a515e268105a63539660646dd
freepose <freepose@126.com>
2025-09-22 18:07:55 +0800

    update

---
+++ c_language_pta_2025/pta_2_4_7.c
@@ -0,0 +1,28 @@
+﻿
+
+#include <stdio.h>
+
+int main()
+{
+    // 利用组合数公式的对称性 (C(n, m) = C(n, n-m)) 来减少计算量。
+    // 以下是一个不使用用户定义函数的C程序，用于计算组合数 (C(n, m)) 并输出结果：
+    int m, n;
+    scanf("%d %d", &m, &n);
+
+    // 确保 m <= n - m 以减少计算量
+    if (m > n - m)
+    {
+        m = n - m;
+    }
+
+    double result = 1.0;
+    for (int i = 0; i < m; i++)
+    {
+        result *= (n - i);
+        result /= (i + 1);
+    }
+
+    printf("result = %.0lf\n", result);
+
+    return 0;
+}
---
+++ c_language_pta_2025/pta_3_10.c
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    double speed, limit;
+
+    scanf("%f %f", &speed, &limit);
+
+
+    // 正常行驶：没有超出限速的10%
+    if (speed < 1.1 * limit)
+    {
+        printf("OK\n");
+    }
+    else if (10. * speed < limit * 15)  
+    {
+        printf("Exceed %.0f%%. Ticket 200\n", 100.0 * (speed - limit) / limit);
+    }
+    else
+    {
+        printf("Exceed %.0f%%. License Revoked\n", 100.0 * (speed - limit) / limit);
+    }
+
+    return 0;
+}
