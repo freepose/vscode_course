0373eada3f5e7883e03ab3e40d4a5b4362a0dbc3
freepose <freepose@126.com>
2025-09-23 09:27:46 +0800

    update

---
+++ c_language_pta_2025/pta_2_1_4.c
@@ -0,0 +1,19 @@
+﻿
+// pta_2_1_4.c 实验2-1-4-表达式 计算平均分
+
+#include <stdio.h>
+
+int main()
+{
+    // int math, eng, comp, average;
+    // math = 87;
+    // eng = 72;
+    // comp = 93;
+    // average = (math + eng + comp) / 3;
+    
+    // printf("average=%d\n", average);
+
+    printf("math = 87, eng = 72, comp = 93, average = %d\n", (87 + 72 + 93) / 3);   // 通过的答案
+
+    return 0;
+}
