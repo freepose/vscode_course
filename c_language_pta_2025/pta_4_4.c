0c58ae21495c0a64e284c5367d0737e8f2510e78
freepose <freepose@126.com>
2025-10-10 09:19:52 +0800

    update

---
+++ c_language_pta_2025/pta_4_4.c
@@ -0,0 +1,23 @@
+﻿
+#include <stdio.h>
+
+int main()
+{
+    int lower, upper;
+    scanf("%d %d", &lower, &upper);
+    if (lower <= 0 || upper <= 0 || lower > upper || upper > 100)
+    {
+        printf("Invalid.\n");
+        return 0;
+    }
+
+    printf("fahr celsius\n");
+
+    for (int f = lower; f <= upper; f += 2)
+    {
+        double c = 5.0 * (f - 32) / 9.0;
+        printf("%d%6.1f\n", f, c);
+    }
+
+    return 0;
+}
