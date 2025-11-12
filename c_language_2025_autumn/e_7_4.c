4ae1b2abd4c1bd6ba0264da9b9b31767b47dd36c
freepose <freepose@126.com>
2025-11-12 08:44:44 +0800

    update

--- c_language_2025_autumn/e_7_4.c
+++ c_language_2025_autumn/e_7_4.c
@@ -6,7 +6,30 @@
 
 int main()
 {
-   
+    int arr[] = {3, 5, 2, 8, 1};
+    int n = sizeof(arr) / sizeof(int);
+
+    int max_value = arr[0]; // initialize max_value with the first element
+    int max_index = 0;      // 下标
+
+    // print array elements
+    for (int i = 0; i < n; i++)
+    {
+        printf("arr[%d] = %d\n", i, arr[i]);
+    }
+
+    // find the maximum value using brute force
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[i] > max_value)
+        {
+            max_value = arr[i];
+            max_index = i;
+        }       
+    }
+
+    printf("Maximum value is %d at index %d\n", max_value, max_index);
+
 
     return 0;
 }
---
+++ c_language_2025_autumn/e_7_5.c
@@ -0,0 +1,48 @@
+﻿
+// Example 7-4: Find the maximum value in an array (Brute Force)
+
+
+#include <stdio.h>
+
+int main()
+{
+    int arr[] = {3, 5, 2, 8, 1};
+    int n = sizeof(arr) / sizeof(int);
+
+    int max_value = arr[0]; // initialize max_value with the first element
+    int max_index = 0;      // 下标
+
+    // print array elements
+    for (int i = 0; i < n; i++)
+    {
+        printf("arr[%d] = %d\n", i, arr[i]);
+    }
+
+    // Selection sort based on finding maximum value
+    for (int i = 0; i < n - 1; i++)
+    {
+       int max_index = i;
+       for (int j = i + 1; j < n; j++)
+       {
+            if (arr[max_index] < arr[j])
+            {
+                max_index = j;
+            }
+       }
+
+       // Swap the found maximum element with the first element
+         if (max_index != i)
+         {
+                int temp = arr[i];
+                arr[i] = arr[max_index];
+                arr[max_index] = temp;
+         }
+    }
+
+        for (int i = 0; i < n; i++)
+    {
+        printf("arr[%d] = %d\n", i, arr[i]);
+    }
+
+    return 0;
+}
