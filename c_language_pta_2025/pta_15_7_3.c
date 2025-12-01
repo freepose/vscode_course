26d825f8cd6fb34bf7cba094d5963ba3b257f39f
freepose <freepose@126.com>
2025-12-01 15:51:28 +0800

    update

---
+++ c_language_pta_2025/pta_15_7_3.c
@@ -0,0 +1,33 @@
+﻿
+
+#include <stdio.h>
+#include <ctype.h>
+
+int main()
+{
+    char s[105] = {0};
+    scanf("%s", s);
+
+    for (char *p = s; *p; p++)
+    {
+        if (isdigit(*p))
+        {
+            int num = 0;
+            while (isdigit(*p))
+            {
+                num = num * 10 + (*p - '0');
+                p++;
+            }
+            for (int i = 0; i < num; i++)
+            {
+                putchar(*p);
+            }
+        }
+        else
+        {
+            putchar(*p);
+        }
+    }
+
+    return 0;
+}
