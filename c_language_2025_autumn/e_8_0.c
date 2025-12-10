c8601cc1cd7ed2f88754910663e3069ff739569e
freepose <freepose@126.com>
2025-12-10 08:43:08 +0800

    update

--- c_language_2025_autumn/e_8_0.c
+++ c_language_2025_autumn/e_8_0.c
@@ -5,27 +5,34 @@
 
 int main()
 {
-    // memory size of data types
-    printf("Size of char: %zu byte(s)\n", sizeof(char));
-    printf("Size of int: %zu byte(s)\n", sizeof(int));
-    printf("Size of float: %zu byte(s)\n", sizeof(float));
-    printf("Size of double: %zu byte(s)\n", sizeof(double));
+    // // memory size of data types
+    // printf("Size of char: %zu byte(s)\n", sizeof(char));
+    // printf("Size of int: %zu byte(s)\n", sizeof(int));
+    // printf("Size of float: %zu byte(s)\n", sizeof(float));
+    // printf("Size of double: %zu byte(s)\n", sizeof(double));
 
-    // memory size of pointers of different data types
-    printf("Size of char*: %zu byte(s)\n", sizeof(char*));
-    printf("Size of int*: %zu byte(s)\n", sizeof(int*));
-    printf("Size of float*: %zu byte(s)\n", sizeof(float*));
-    printf("Size of double*: %zu byte(s)\n", sizeof(double*));
+    // // memory size of pointers of different data types
+    // printf("Size of char*: %zu byte(s)\n", sizeof(char*));
+    // printf("Size of int*: %zu byte(s)\n", sizeof(int*));
+    // printf("Size of float*: %zu byte(s)\n", sizeof(float*));
+    // printf("Size of double*: %zu byte(s)\n", sizeof(double*));
 
-    int n = 5, *p;
+    // int n = 5, *p;
 
-    p = &n; // address of variable n is assigned to pointer p
-    printf("%p\n", p);
-    printf("%d\n", *p); // value of n through pointer p
+    // p = &n; // address of variable n is assigned to pointer p
+    // printf("%p\n", p);
+    // printf("%d\n", *p); // value of n through pointer p
 
-    // If we change *p, the value of n will also change
-    *p = 10;
-    printf("%d\n", n);
+    // // If we change *p, the value of n will also change
+    // *p = 10;
+    // printf("%d\n", n);
+
+    int n = 20;
+    int *p = &n;
+
+    printf("Address of n: %p\n", (void*)&n);
+    *p = 30;
+    printf("Updated value of n: %d\n", n);
 
     return 0;
 }
