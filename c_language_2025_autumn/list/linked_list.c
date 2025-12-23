95f3e266ab137f4eca8852025a83dec69ebd4279
freepose <freepose@126.com>
2025-12-23 16:49:04 +0800

    list

---
+++ c_language_2025_autumn/list/linked_list.c
@@ -0,0 +1,115 @@
+﻿
+// 线性表之二：链表
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct _node
+{
+    int data;
+    struct _node* next;
+};
+
+typedef struct _node Node;
+
+
+Node* create_linked_list(int arr[], int n)
+{
+    // 头插法
+    Node *head = 0;
+    Node *p = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        p = (Node*)malloc(sizeof(Node));
+        p->data = arr[i];
+
+        p->next = head;
+        head = p;
+    }
+
+    return head;
+}
+
+Node *create_linked_list_tail(int arr[], int n)
+{
+    // 尾插法
+    Node *head = 0;
+    Node *tail = 0;
+    Node *p = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        p = (Node*)malloc(sizeof(Node));
+        p->data = arr[i];
+        p->next = 0;
+
+        if (head == 0)
+        {
+            head = p;
+            tail = p;
+        }
+        else
+        {
+            tail->next = p;
+            tail = p;
+        }
+    }
+
+    return head;
+}
+
+void print_linked_list(Node* head)
+{
+    Node* p = head;
+    while (p != 0)
+    {
+        printf("%d -> ", p->data);
+        p = p->next;
+    }
+    printf("NULL\n");
+}
+
+void insert_element(Node** head, int index, int value)
+{
+    Node* p = (Node*)malloc(sizeof(Node));
+    p->data = value;
+
+    if (index == 0)
+    {
+        p->next = *head;
+        *head = p;
+        return;
+    }
+
+    Node* curr = *head;
+    for (int i = 0; i < index - 1 && curr != 0; i++)
+    {
+        curr = curr->next;
+    }
+
+    if (curr == 0)
+    {
+        free(p);
+        return;
+    }
+
+    p->next = curr->next;
+    curr->next = p;
+}
+
+
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5};
+    Node* head = 0;
+
+    // head = create_linked_list(arr, 5);
+    head = create_linked_list_tail(arr, 5);
+    print_linked_list(head);
+
+    insert_element(&head, 2, 99);
+    print_linked_list(head);
+
+    return 0;
+}
---
+++ c_language_2025_autumn/list/seq_list.c
@@ -0,0 +1,68 @@
+﻿
+// 线性表之一：顺序表
+
+#include <stdio.h>
+#include <string.h>
+
+typedef struct _student
+{
+    char name[50];
+    int age;
+    float score;
+} Student;
+
+typedef struct _seq_list
+{
+    Student data[100];
+    int length;
+} SeqList;
+
+
+void init_seq_list(SeqList* list)
+{
+    memset(list, 0, sizeof(SeqList));
+}
+
+void create_seq_list(SeqList* list, Student arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        list->data[i] = arr[i];
+    }
+    list->length = n;
+}
+
+void delete_element(SeqList* list, int index)
+{
+    if (index < 0 || index >= list->length)
+    {
+        return;
+    }
+
+    for (int i = index; i < list->length - 1; i++)
+    {
+        list->data[i] = list->data[i + 1];
+    }
+    list->length--;
+}
+
+void append_element(SeqList* list, Student element)
+{
+    if (list->length >= 100)
+    {
+        return;
+    }
+
+    list->data[list->length] = element;
+    list->length++;
+}
+
+int main()
+{
+    SeqList student_list;
+
+    init_seq_list(&student_list);
+
+    return 0;
+}
+
